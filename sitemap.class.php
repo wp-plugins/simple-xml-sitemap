1bc124d9ac74510e53ead8e6e8abe64a54e761b3
gregmolnar <gregmolnar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-29 21:17:01 +0000

    1.1 released
    git-svn-id: https://plugins.svn.wordpress.org/simple-xml-sitemap/tags/1.1@223122 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sitemap.class.php
+++ sitemap.class.php
@@ -12,7 +12,7 @@ class sitemap
     
     function build()
     {
-        $mem_start = $this->memory_usage();
+        $mem_start = $this->memory_usage(); 
         $priority = get_option('priority');
         $output = '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
